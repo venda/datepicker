386ad0d751ea2ceac48f82fe216318e6aeaa7298
awillis@fastmail.fm <andrewwillis@venda.com>
2014-03-26 17:57:22 +0000

    First commit

---
+++ app/scripts/main.js
@@ -0,0 +1,39 @@
+Venda.Datepicker.init({
+
+  datepickerContainer: '.container',
+  selectContainer: '.select',
+  nextDayDelivery: true,
+
+  // 24 hr.
+  nddCutoffTime: 15,
+
+  useRange: true,
+  rangeInDays: 14,
+
+  // 0 or 1
+  firstDayDiff: 1,
+  largeDeviceSmoothScroll: true,
+  scrollSpeed: 60,
+  timeSeparationInMinutes: 30,
+  showTimes: true,
+  startHour: 9,
+  endHour: 22,
+
+  // 24hr
+//  usePreselectedTime: false,
+//  preSelectedTime: 1000,
+
+  inactive: {
+    monthsStartAtZero: true,
+    dates: [
+      { year: 2014, month: 8, day: 6 },
+      { year: 2014, month: 2, day: 6 },
+      { year: 2014, month: 2, day: 11 },
+      { year: 2014, month: 2, day: 20 },
+      { year: 2014, month: 3, day: 10 },
+      { year: 2014, month: 3, day: 2 },
+      { year: 2014, month: 4, day: 12 }
+    ]
+  }
+
+});
