409222946a5403c97f97d5f23cbcf77d06b7de8a
awillis@fastmail.fm <andrewwillis@venda.com>
2014-04-03 16:49:52 +0100

    Corrected some documentation.

---
+++ app/scripts/example.js
@@ -0,0 +1,26 @@
+Datepicker.init({
+  language: 'en',
+  region: 'england-and-wales',
+  formInputName: 'selecteddatetime',
+  deactivateBankHolidays: true,
+  nextDayDelivery: true,
+  hideSelectsOnDatePicker: false,
+  nddCutoffTime: 13,
+  useRange: false,
+  rangeInDays: 2,
+  highlightWeekends: false,
+  firstDayDiff: 1,
+  largeDeviceSmoothScroll: true,
+  scrollSpeed: 60,
+  timeSeparationInMinutes: 30,
+  showTimes: true,
+  startHour: 9,
+  endHour: 22,
+  inactive: {
+    monthsStartAtZero: false,
+    blockDay: [6],
+    dates: [
+      '2014-02-30', '2014-04-11'
+    ]
+  }
+});
